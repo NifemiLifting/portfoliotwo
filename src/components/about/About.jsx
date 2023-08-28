d714cb3d65f9934fe91787489c0a0606d2a5ba52
Guest+ <guestp@Nifemi-MacBook-Pro.local>
2023-08-28 22:11:09 +0100

    finals

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <img src={photo} alt='photo' className='header-image' />
       </div>
       <div className='ovo__about-content section__margin'>
-        <h1 >Get to know ovo</h1>
+        <h1 >Get to know Ovo</h1>
         <p>
         Currently working with eSonar Inc. as a Junior Electronics Engineer. An Electrical Engineer-In-Training (E.I.T.) with approximately 2 years of experience in the electronics, power, construction, and communication field. I am capable of working independently with minimum supervision and in a team setting. Dependable and reliable, ready to learn and use my skills to grow with your company.
         </p>
