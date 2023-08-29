4634c71a3d31ee3014b46a724f164a8a21ac098d
Guest+ <guestp@Nifemi-MacBook-Pro.local>
2023-08-29 16:36:28 +0100

    finals

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,12 +9,12 @@ const Projects = () => {
           <img src={light}/>
         </div>
       </div>
-      <div className='ovo__projects-grid'>
-        <div className='grid-item'>
+      <div className='ovo__projects-grid scale-up-center'>
+        <div className='grid-item gradient__text'>
             <h1>SKILLS</h1>
             
-            <p>Microchip Studio, MS Office </p>
-            <p>Altium, Eagle </p>
+            <p>Microchip Studio and MS Office </p>
+            <p>Altium and Eagle </p>
             <p>Fusion360 </p>
             <p>AUTOCAD certified user</p>
             <p>C/C++ and Python</p>
@@ -24,12 +24,12 @@ const Projects = () => {
             <p> PLC Programming </p>
 
           </div>
-          <div className='grid-item'>
+          <div className='grid-item gradient__text'>
             <h1>PROJECTS</h1>
             <p>Electrical Team lead - Tidal Turbine Power Generation Analysis Project</p>
             <p>Worked with a multidisciplinary team of Engineering students on Tidal Turbine Power Generation analysis using MAT-LAB/Simulink and ANSYS analysis.</p>
-            <p>• Used Simulink to design the mechanical turbine design based on calculations made</p>
-            <p> Prepared meeting minutes, presentation materials and project reports for the project</p>
+            <p>Used Simulink to design the mechanical turbine design based on calculations made</p>
+            <p>Prepared meeting minutes, presentation materials and project reports for the project</p>
             <p><a href='https://onedrive.live.com/edit.aspx?resid=BE13BD5DDD2399DD!2766&ithint=file%2cpptx&authkey=!AMwyJ5V1ncTJQFA'>Capstone Project</a></p>
           </div>
       </div>
