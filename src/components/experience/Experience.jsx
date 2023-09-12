abe05cadae5862c173b951d99b80bee0f72f2b0a
Guest+ <guestp@Nifemi-MacBook-Pro.local>
2023-09-12 13:25:51 +0100

    final commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -22,7 +22,7 @@ const Experience = () => {
            
             </div>
             
-            <p><span className='date'>April 2023 till present</span><br/>Collaborate on software component integration, develop schematics, PCB layouts, documentation, collaborate with Electrical team, mechanical, and software engineers for cost-effective design requirements.</p>
+            <p><span className='date'>April 2023 till present</span><br/><br/>Collaborate on software component integration, develop schematics, PCB layouts, documentation, collaborate with Electrical team, mechanical, and software engineers for cost-effective design requirements.</p>
           </div>
         </div>
         <div className='experience-item'>
@@ -34,7 +34,7 @@ const Experience = () => {
             
             </div>
             
-            <p><span className='date'>October 2022 - April 2023</span><br/>Manage project work flow, trafficking, and communication to maintain deadlines. Coordinate material movement, schedule deliveries, and ensure compliance with material requests. Utilize Excel and Hilti for project scheduling and inventory. Advise management of progress and product delays.</p>
+            <p><span className='date'>October 2022 - April 2023</span><br/><br/>Manage project work flow, trafficking, and communication to maintain deadlines. Coordinate material movement, schedule deliveries, and ensure compliance with material requests. Utilize Excel and Hilti for project scheduling and inventory. Advise management of progress and product delays.</p>
           </div>
         </div>
         <div className='experience-item'>
@@ -50,7 +50,7 @@ const Experience = () => {
      
           </div>
           
-          <p><span className='date'>September 2021 - December 2021</span><br/>Using a risk-based inspection approach, evaluate the moisture danger for 54 high-voltage terminal stations with power transformers ranging from 20 kVA to 250 MVA. Give engineering support, visit the site, and offer designs and maintenance budget estimates. To ensure transformer stability and efficiency, remotely activate alerts and keep an organised database of power transformer loading information.</p>
+          <p><span className='date'>September 2021 - December 2021</span><br/><br/>Using a risk-based inspection approach, evaluate the moisture danger for 54 high-voltage terminal stations with power transformers ranging from 20 kVA to 250 MVA. Give engineering support, visit the site, and offer designs and maintenance budget estimates. To ensure transformer stability and efficiency, remotely activate alerts and keep an organised database of power transformer loading information.</p>
           </div>
         </div>
         <div className='experience-item'>
@@ -63,7 +63,7 @@ const Experience = () => {
           
           </div>
          
-          <p><span className='date'>January 2021 - April 2021</span><br/>Weekly reports were prepared, a Python code was developed for a double pole thermostat mechanical switch, and specifications were communicated. Weekly meetings were held with the engineering team. Plans, designs, and analysis were prepared for testing, and product acceptance tests were performed using Python. Prusa 3D printers were used for parts creation and maintenance.</p>
+          <p><span className='date'>January 2021 - April 2021</span><br/><br/>Weekly reports were prepared, a Python code was developed for a double pole thermostat mechanical switch, and specifications were communicated. Weekly meetings were held with the engineering team. Plans, designs, and analysis were prepared for testing, and product acceptance tests were performed using Python. Prusa 3D printers were used for parts creation and maintenance.</p>
         </div>
         </div>
         <div className='experience-item'>
@@ -74,7 +74,7 @@ const Experience = () => {
            
           </div>
            
-           <p><span className='date'>May 2020 - August 2020</span><br/>Created Bill of material (BOM) for Fresnel lens project, sourced components, participated in firmware development, designed PCB board, and worked with Arduino IDE software. Assembled Prusa mini 3D printers and printed models for ongoing projects.</p>
+           <p><span className='date'>May 2020 - August 2020</span><br/><br/>Created Bill of material (BOM) for Fresnel lens project, sourced components, participated in firmware development, designed PCB board, and worked with Arduino IDE software. Assembled Prusa mini 3D printers and printed models for ongoing projects.</p>
           </div>
         </div>
    
